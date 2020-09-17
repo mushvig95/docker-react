4db12815551b9958926354513556138ff270d7a2
mushvig-niftaliyev <mushvig.niftaliyev@pashabank.az>
2020-09-17 12:28:26 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          lolo there from react app
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 8
         </a>
       </header>
     </div>
